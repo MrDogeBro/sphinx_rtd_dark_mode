419639f63c5c3c76876bc009b7e925a74db65510
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-04-29 18:32:49 -0400

    Add init file so sphinx can use the folder for the extension and move setup here

---
+++ sphinx_rtd_dark_mode/__init__.py
@@ -0,0 +1,26 @@
+"""
+Sphinx Read the Docs Dark Mode
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Allows for toggable dark mode on the Read the Docs
+theme for Sphinx.
+"""
+
+__title__ = "sphinx-rtd-dark-mode"
+__description__ = "Dark mode for the Sphinx Read the Docs theme."
+__author__ = "MrDogeBro"
+__version__ = "1.0.0"
+
+from sphinx_rtd_dark_mode.dark_mode_loader import DarkModeLoader
+
+
+def setup(app):
+    app.add_config_value("default_dark_mode", True, "html")
+
+    app.connect("config-inited", DarkModeLoader().configure)
+
+    return {
+        "version": "1.0.0",
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }
