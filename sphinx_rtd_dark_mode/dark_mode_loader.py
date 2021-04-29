cd0c35a4ac2f6fc4925de327cda64587b50447a7
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-04-29 18:45:53 -0400

    Append to config if already specified else just write

--- sphinx_rtd_dark_mode/dark_mode_loader.py
+++ sphinx_rtd_dark_mode/dark_mode_loader.py
@@ -8,32 +8,40 @@ class DarkModeLoader:
     def configure(self, app, config):
         self.config = config
 
-        self.config.html_static_path = [
-            str(Path.joinpath(Path(__file__).resolve().parent, "static"))
-        ]
+        if not self.config.html_static_path:
+            self.config.html_static_path = [
+                str(Path.joinpath(Path(__file__).resolve().parent, "static"))
+            ]
+        else:
+            self.config.html_static_path.append(
+                str(Path.joinpath(Path(__file__).resolve().parent, "static"))
+            )
 
         if not self.config.default_dark_mode:
-            self.load_default_light()
+            self.load_default_theme("light")
+            self.load_css()
             return
 
-        self.load_default_dark()
-
-    def load_default_dark(self):
-        self.config.html_js_files = [
-            "dark_mode_js/default_dark.js",
-            "dark_mode_js/theme_switcher.js",
-        ]
+        self.load_default_theme("dark")
         self.load_css()
 
-    def load_default_light(self):
-        self.config.html_js_files = [
-            "dark_mode_js/default_light.js",
-            "dark_mode_js/theme_switcher.js",
-        ]
-        self.load_css()
+    def load_default_theme(self, default_theme: str):
+        if not self.config.html_js_files:
+            self.config.html_js_files = [
+                f"dark_mode_js/default_{default_theme}.js",
+                "dark_mode_js/theme_switcher.js",
+            ]
+        else:
+            self.config.html_js_files.append(f"dark_mode_js/default_{default_theme}.js")
+            self.config.html_js_files.append("dark_mode_js/theme_switcher.js")
 
     def load_css(self):
-        self.config.html_css_files = [
-            "dark_mode_css/custom.css",
-            "dark_mode_css/dark.css",
-        ]
+        if not self.config.html_css_files:
+            self.config.html_css_files = [
+                "dark_mode_css/custom.css",
+                "dark_mode_css/dark.css",
+            ]
+        else:
+            self.config.html_css_files.append("dark_mode_css/custom.css")
+            self.config.html_css_files.append("dark_mode_css/dark.css")
+        print(self.config.html_css_files)
