275637671f38c0208fc4f30abdc8ead4a21a91a2
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-04-29 18:25:03 -0400

    Wait to read config until builder has intialized config

--- sphinx_rtd_dark_mode/sphinx_rtd_dark_mode.py
+++ sphinx_rtd_dark_mode/sphinx_rtd_dark_mode.py
@@ -1,37 +1,39 @@
 from pathlib import Path
 
 
-class SphinxRTDDarkMode:
-    def __init__(self, app):
-        self.app = app
+class DarkModeLoader:
+    def __init__(self):
+        self.config = None
 
-    def configure(self):
-        self.app.config.html_static_path = [
+    def configure(self, app, config):
+        self.config = config
+
+        self.config.html_static_path = [
             str(Path.joinpath(Path(__file__).resolve().parent, "static"))
         ]
 
-        if not self.app.config.default_dark_mode:
+        if not self.config.default_dark_mode:
             self.load_default_light()
             return
 
         self.load_default_dark()
 
     def load_default_dark(self):
-        self.app.config.html_js_files = [
+        self.config.html_js_files = [
             "dark_mode_js/default_dark.js",
             "dark_mode_js/theme_switcher.js",
         ]
         self.load_css()
 
     def load_default_light(self):
-        self.app.config.html_js_files = [
+        self.config.html_js_files = [
             "dark_mode_js/default_light.js",
             "dark_mode_js/theme_switcher.js",
         ]
         self.load_css()
 
     def load_css(self):
-        self.app.config.html_css_files = [
+        self.config.html_css_files = [
             "dark_mode_css/custom.css",
             "dark_mode_css/dark.css",
         ]
@@ -40,7 +42,7 @@ class SphinxRTDDarkMode:
 def setup(app):
     app.add_config_value("default_dark_mode", True, "html")
 
-    SphinxRTDDarkMode(app).configure()
+    app.connect("config-inited", DarkModeLoader().configure)
 
     return {
         "version": "1.0.0",
