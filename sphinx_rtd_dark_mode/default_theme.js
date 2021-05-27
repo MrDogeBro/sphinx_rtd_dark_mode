6297b8b48f301b06abecc6085d10998eead2e7ea
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-05-26 20:14:30 -0400

    Set data theme to whatever theme is selected and enable theming if theme doesn't equal light

--- sphinx_rtd_dark_mode/default_theme.js
+++ sphinx_rtd_dark_mode/default_theme.js
@@ -2,10 +2,10 @@ const loadTheme = () => {
   let theme = localStorage.getItem('theme');
 
   if (theme !== null) {
-    if (theme === 'dark') {
-      document.documentElement.setAttribute('data-theme', 'dark');
+    if (theme !== 'light')
       document.documentElement.setAttribute('data-theme-enabled', 'true');
-    }
+
+    document.documentElement.setAttribute('data-theme', theme);
   } else {
     localStorage.setItem('theme', '{default_theme}');
     document.documentElement.setAttribute('data-theme', '{default_theme}');
