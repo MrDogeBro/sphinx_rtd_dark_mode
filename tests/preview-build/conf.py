09ae5f309d21d9c585835e925b715faee1c8740b
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-05-29 17:07:32 -0400

    Add extensions and modules needed for preview docs

--- tests/preview-build/conf.py
+++ tests/preview-build/conf.py
@@ -1,10 +1,17 @@
 import os
 import sys
+import content_filter
 
 sys.path.insert(0, os.path.abspath("../.."))
 
 extensions = [
     "sphinx_rtd_dark_mode",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
 ]
+
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_theme = "sphinx_rtd_theme"
