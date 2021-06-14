4f44b96441d35d041fbf029c6bdb2baba4095270
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-06-14 09:49:12 -0400

    Add handling for all events, correctly stop on ctrl+c, and move observer to class var

--- dev/dev-server/watch.py
+++ dev/dev-server/watch.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime as dt, timedelta
 from pathlib import Path
 from time import sleep
 from watchdog.observers import Observer
@@ -7,15 +7,13 @@ from watchdog.events import FileSystemEventHandler
 
 class EventHandler(FileSystemEventHandler):
     def __init__(self):
-        self.last_modified = datetime.now()
+        self.last_modified = dt.now()
 
-    def on_modified(self, event):
-        if datetime.now() - self.last_modified < timedelta(seconds=1):
+    def on_any_event(self, event):
+        if dt.now() - self.last_modified < timedelta(seconds=1):
             return
         else:
-            self.last_modified = datetime.now()
-
-        print(event)
+            self.last_modified = dt.now()
 
 
 class Watcher:
@@ -23,17 +21,19 @@ class Watcher:
         self.source_path = source_path
         self.docs_path = docs_path
 
+        self.observer = Observer()
+
     def watch(self):
         path = Path.joinpath(Path(__file__).resolve().parent, self.source_path)
         event_handler = EventHandler()
-        observer = Observer()
 
-        observer.schedule(event_handler, path, recursive=True)
-        observer.start()
+        self.observer.schedule(event_handler, path, recursive=True)
+        self.observer.start()
 
         try:
             while True:
                 sleep(1)
+        except KeyboardInterrupt:
+            self.observer.stop()
         finally:
-            observer.stop()
-            observer.join()
+            self.observer.join()
