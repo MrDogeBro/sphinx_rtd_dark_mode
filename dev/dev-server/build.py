b860e6e03d97058ffbec50c218c5e3861cd3ad7d
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-06-14 10:20:27 -0400

    Add class to contain build output

--- dev/dev-server/build.py
+++ dev/dev-server/build.py
@@ -1,6 +1,20 @@
 from pathlib import Path
 
 
+class BuildOutput:
+    def __init__(self, traceback: str, status: int) -> None:
+        self.traceback = traceback
+        self.status = status
+
+    @property
+    def traceback(self) -> str:
+        return self.traceback
+
+    @property
+    def status(self) -> int:
+        return self.status
+
+
 class Builder:
     def __init__(self, docs_path: str) -> None:
         self.build_path = Path.joinpath(Path(__file__).resolve().parent, ".dev-build")
