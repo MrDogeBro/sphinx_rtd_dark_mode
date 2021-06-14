199833e4a8c7ba3a6564e63896e76f35024e8895
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-06-14 13:03:41 -0400

    Add code for building preview

--- dev/dev-server/build.py
+++ dev/dev-server/build.py
@@ -1,4 +1,9 @@
+from distutils.dir_util import copy_tree, remove_tree
+from os import chdir
 from pathlib import Path
+from subprocess import run as run_cmd, PIPE
+from tempfile import TemporaryDirectory
+from time import sleep
 
 
 class BuildOutput:
@@ -16,9 +21,35 @@ class BuildOutput:
 
 
 class Builder:
-    def __init__(self, docs_path: str) -> None:
-        self.build_path = Path.joinpath(Path(__file__).resolve().parent, ".dev-build")
+    def __init__(self, source_path: str, docs_path: str) -> None:
+        self.output_path = Path.joinpath(
+            Path(__file__).resolve().parent, "../.dev-build"
+        )
+        self.source_path = source_path
         self.docs_path = docs_path
+        self.in_progress = False
 
-    def build(self) -> str:
-        pass
+    def build(self) -> BuildOutput:
+        if self.in_progress:
+            return
+
+        self.in_progress = True
+
+        build_source_path = str(Path.joinpath(self.output_path, "source"))
+        build_docs_path = str(Path.joinpath(self.output_path, "docs"))
+
+        remove_tree(self.output_path)
+
+        copy_tree(self.source_path, build_source_path)
+        copy_tree(self.docs_path, build_docs_path)
+
+        build_cmd = run_cmd(
+            ["sphinx-build", "docs", "build"],
+            stdout=PIPE,
+            cwd=str(self.output_path),
+        )
+
+        build_traceback = build_cmd.stdout.decode("utf-8")
+        self.in_progress = False
+
+        return BuildOutput(build_traceback, build_cmd.returncode)
