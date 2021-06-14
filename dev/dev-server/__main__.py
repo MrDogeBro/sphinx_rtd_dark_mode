0554a2b98745e77b91ca562428d66000492f7f8a
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-06-13 20:44:51 -0400

    Add file to wrap everything together

---
+++ dev/dev-server/__main__.py
@@ -0,0 +1,23 @@
+from argparse import ArgumentParser
+from pathlib import Path
+
+from watch import Watcher
+
+
+def main():
+    cwd = Path().cwd()
+
+    parser = ArgumentParser()
+    parser.add_argument("source_path")
+    parser.add_argument("docs_path")
+    args = parser.parse_args()
+
+    source_path = Path.joinpath(cwd, args.source_path)
+    docs_path = Path.joinpath(cwd, args.docs_path)
+
+    watcher = Watcher(source_path, docs_path)
+    watcher.watch()
+
+
+if __name__ == "__main__":
+    main()
