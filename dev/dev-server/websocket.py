b389d640ba839586e94f121db6c1bd79ddaf1994
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-06-15 18:20:22 -0400

    Add ability to stop websocket server

--- dev/dev-server/websocket.py
+++ dev/dev-server/websocket.py
@@ -3,13 +3,21 @@ from websockets import serve as websocket_serve
 
 
 class Websocket:
-    async def handler(self, websocket, path):
+    def __init__(self) -> None:
+        self.running = False
+
+    async def handler(self, websocket, path) -> None:
         async for message in websocket:
             await websocket.send(message)
 
-    async def async_start(self):
+    async def async_start(self) -> None:
+        self.running = True
         async with websocket_serve(self.handler, "localhost", 5010):
-            await asyncio.Future()
+            while self.running:
+                await asyncio.sleep(1)
 
-    def start(self):
+    def start(self) -> None:
         asyncio.run(self.async_start())
+
+    async def stop(self) -> None:
+        self.running = False
