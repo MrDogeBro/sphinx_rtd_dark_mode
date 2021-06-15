1022a20c5a624c85fd902ea0a536c3ff40ac1e19
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-06-15 17:21:30 -0400

    Use async for watcher

--- dev/dev-server/__main__.py
+++ dev/dev-server/__main__.py
@@ -1,24 +1,39 @@
-from asyncio import run as async_run
+import asyncio
 from argparse import ArgumentParser
 from pathlib import Path
+from signal import signal, SIGINT
 
 from watch import Watcher
 
 
-async def main():
-    cwd = Path().cwd()
+class Main:
+    async def start(self) -> None:
+        cwd = Path().cwd()
 
-    parser = ArgumentParser()
-    parser.add_argument("source_path")
-    parser.add_argument("docs_path")
-    args = parser.parse_args()
+        parser = ArgumentParser()
+        parser.add_argument("source_path")
+        parser.add_argument("docs_path")
+        args = parser.parse_args()
 
-    source_path = Path.joinpath(cwd, args.source_path)
-    docs_path = Path.joinpath(cwd, args.docs_path)
+        source_path = Path.joinpath(cwd, args.source_path)
+        docs_path = Path.joinpath(cwd, args.docs_path)
 
-    watcher = Watcher(source_path, docs_path)
-    watcher.watch()
+        self.watcher = Watcher(str(source_path), str(docs_path))
+
+        signal(SIGINT, self.stop)
+
+        await self.watcher.start()
+
+    def stop(self, signal_received, frame) -> None:
+        loop = asyncio.get_running_loop()
+        loop.create_task(self.async_stop())
+
+    async def async_stop(self) -> None:
+        await self.watcher.stop()
+        await asyncio.sleep(1)
+
+        exit(0)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(Main().start())
