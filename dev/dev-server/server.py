d40c75d0b67a71e539c38b8adda64671892b3a4a
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-06-15 21:28:48 -0400

    Adjust path to go to build folder by default and handle files that cant be decoded into utf-8

--- dev/dev-server/server.py
+++ dev/dev-server/server.py
@@ -5,22 +5,41 @@ from threading import Thread
 
 class ServerHandler(BaseHTTPRequestHandler):
     def do_GET(self) -> None:
+        custom_path = False
+
         if self.path == "/":
             self.path = "index.html"
         elif self.path == "/default/static/404.css":
             self.path = str(
                 Path.joinpath(Path(__file__).resolve().parent, "static/404.css")
             )
+            custom_path = True
         elif self.path == "/default/static/websocket.js":
             self.path = str(
                 Path.joinpath(Path(__file__).resolve().parent, "static/websocket.js")
             )
+            custom_path = True
         else:
             self.path = self.path[1:]
+            if self.path.count("?") > 0:
+                self.path = self.path[: self.path.rfind("?")]
 
         try:
-            with open(self.path) as f:
-                requested_file = f.read()
+            adjusted_path = None
+            is_bytes = False
+
+            if not custom_path:
+                adjusted_path = self.output_path = Path.joinpath(
+                    Path(__file__).resolve().parent, "../.dev-build/build", self.path
+                )
+
+            try:
+                with open(adjusted_path if adjusted_path else self.path) as f:
+                    requested_file = f.read()
+            except UnicodeDecodeError:
+                with open(adjusted_path if adjusted_path else self.path, "rb") as f: # type: ignore[assignment]
+                    # requested_file = f.read()
+                    is_bytes = True
 
             self.send_response(200)
         except FileNotFoundError:
@@ -31,17 +50,24 @@ class ServerHandler(BaseHTTPRequestHandler):
 
             self.send_response(404)
 
-        if "<head>" in requested_file:
-            requested_file = requested_file.replace(
-                "<head>", f'<head><script src="default/static/websocket.js"></script>'
-            )
-        else:
-            requested_file = requested_file.replace(
-                "<html>",
-                f'<html><head><script src="default/static/websocket.js"></script></head>',
-            )
+        if not is_bytes and self.path.endswith(".html"):
+            if "<head>" in requested_file:
+                requested_file = requested_file.replace(
+                    "<head>",
+                    f'<head><script src="default/static/websocket.js"></script>',
+                )
+            else:
+                requested_file = requested_file.replace(
+                    "<html>",
+                    f'<html><head><script src="default/static/websocket.js"></script></head>',
+                )
 
         self.end_headers()
+
+        if is_bytes and isinstance(requested_file, bytes):
+            self.wfile.write(requested_file)
+            return
+
         self.wfile.write(bytes(requested_file, "utf-8"))
 
 
