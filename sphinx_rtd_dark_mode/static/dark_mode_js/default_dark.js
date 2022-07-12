fe1e8b430d4a08cb56ae4348626f8afeffe223b7
RKJ <jrohit1110@gmail.com>
2022-07-12 14:14:47 +0530

    default_dark_mode = False, works now

--- sphinx_rtd_dark_mode/static/dark_mode_js/default_dark.js
+++ sphinx_rtd_dark_mode/static/dark_mode_js/default_dark.js
@@ -2,8 +2,8 @@ const loadTheme = () => {
   let theme = localStorage.getItem('theme');
 
   if (theme !== null) {
-    if (theme === 'dark')
-      document.documentElement.setAttribute('data-theme', 'dark');
+    if (theme === 'light')
+      document.documentElement.setAttribute('data-theme', 'light');
   } else {
     localStorage.setItem('theme', 'dark');
     document.documentElement.setAttribute('data-theme', 'dark');
